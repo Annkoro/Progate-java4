28af77c0ac6052323ffd61d09b79e90275a8de17
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-07 23:08:53 +0900

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,5 +1,11 @@
 class Person {
   public String name;
+  
+  //　コンストラクタの定義
+  Person() {
+    System.out.println("インスタンスが生成されました");
+  }
+  
   public void hello() {
     System.out.println("こんにちは、私は" + this.name + "です");
   }
