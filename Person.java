5760c9cb30be7dbbe187190be8e0440daaa25f72
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-07 23:16:00 +0900

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,9 +1,11 @@
 class Person {
   public String name;
   
-  //　コンストラクタの定義
-  Person() {
+  //　コンストラクタの定義、String型の引数を受け取る
+  Person(String name) {
     System.out.println("インスタンスが生成されました");
+    //インスタンスフィールドnameに値をセットする
+      this.name = name;
   }
   
   public void hello() {
