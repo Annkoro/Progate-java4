27d973889ba8a1d9d9bd77bc6c923c74e0858b77
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-07 23:09:25 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 class Main {
   public static void main(String[] args) {
     Person person1 = new Person();
-    person1.hello();
-    person1.name = "kate Jones";
     System.out.println(person1.name);
     Person person2 = new Person();
-    person2.hello();
-    person2.name = "John Christopher Smith";
-    System.out.println(person2.name);
   }
 }
