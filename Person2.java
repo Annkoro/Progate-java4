8500970bece2e25ac834949406157c726908721c
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-09 22:50:18 +0900

    Update Person2.java

--- Person2.java
+++ Person2.java
@@ -20,4 +20,10 @@ class Person {
   public double bmi() {
     return this.weight/height/height;
   }
+  
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+  }
 }
