ef44dddf763e2b0193d374036067dd3330deff0f
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-10 21:39:38 +0900

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,7 +1,10 @@
 class Main {
   public static void main(String[] args) {
     Person person1 = new Person("kate", "Jones", 27, 1.6, 50.0);
-    
     person1.printData();
+    Person person2 = new Person("John", "Smith", 65, 1.75, 80.0);
+    person2.printData();
+    
+    System.out.println("合計" + Person.count + "人です");
   }
 }
