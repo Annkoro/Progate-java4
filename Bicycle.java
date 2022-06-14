9ac54a4a11327d7e2f36f095fa02d129fbf0cf32
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-14 17:26:26 +0900

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -1,7 +1,9 @@
 class Bicycle {
   private String name;
+  private String color;
   Bicycle(String name, String color) {
     this.name = name;
+    this.color = color;
   }
   public void printData() {
     System.out.println("名前：" + this.name);
