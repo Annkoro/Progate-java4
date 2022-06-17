3e182920f8e48577f76c17dee52492afe34254ca
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-17 21:18:58 +0900

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -1,6 +1,7 @@
 class Bicycle {
   private String name;
   private String color;
+  private int distance = 0;
   Bicycle(String name, String color) {
     this.name = name;
     this.color = color;
@@ -8,5 +9,6 @@ class Bicycle {
   public void printData() {
     System.out.println("名前：" + this.name);
     System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
   }
 }
