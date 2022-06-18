0d62a1d963e9dc10494f2b75252087cd2d4f89f4
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-18 20:49:51 +0900

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     Bicycle bicycle = new Bicycle("ビアンキ", "緑");
     System.out.println("【自転車の情報】");
     bicycle.printData();
     System.out.println("-----------------");
-    bicycle.run(10);
+    System.out.print("走る距離を入力してください：");
+    int bicycleDistance = scanner.nextInt();
+    bicycle.run(bicycleDistance);
   }
 }
