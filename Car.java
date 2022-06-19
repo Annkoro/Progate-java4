c2b4edbe859460b6c0b40c86be4fcc1503e4b20b
Annkoro <96101314+Annkoro@users.noreply.github.com>
2022-06-19 22:30:37 +0900

    Update Car.java

--- Car.java
+++ Car.java
@@ -27,4 +27,16 @@ class Car {
     System.out.println("走行距離：" + this.distance + "km");
     System.out.println("ガソリン量：" + this.fuel + "L");
   }
+  public void charge(int litre) {
+    System.out.println(litre + "L給油します");
+    if (litre <= 0) {
+      System.out.println("給油できません");
+      } else if (litre + this.fuel >= 100) {
+      System.out.println("満タンまで給油します");
+        this.fuel = 100;
+      } else {
+        this.fuel += litre;
+      }
+    System.out.println("ガソリン量：" + this.fuel + "L");
+    }
 }
